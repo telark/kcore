10c9df3007ab71bc053516b78c1c936b0b22b8b5
hourki <houssem.kraoua@gmail.com>
2026-03-19 21:42:16 +0100

    cleanup

---
+++ constants/config.go
@@ -0,0 +1,13 @@
+package constants
+
+const (
+	EmptyString        = ""
+	EmptySliceLength   = 0
+	ZeroValue          = 0
+	SingleItem         = 1
+	WorkerPoolAddCount = 1
+)
+
+const (
+	APIVersionV1 = "v1"
+)
