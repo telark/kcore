33d0fdc1487f7d12ab36bce93068fe2340ae7ce8
hourki <houssem.kraoua@gmail.com>
2026-05-12 23:17:39 +0200

    fix

--- constants/retry.go
+++ constants/retry.go
@@ -13,11 +13,11 @@ const (
 	K8sApplyRetryFactor              = 2.0
 	K8sApplyRetryJitter              = 0.2
 	K8sApplyRetryCap                 = 1 * time.Second
-	K8sClientRateLimitWaitSub        = "client rate limiter Wait"
 	K8sReachabilityProbeTimeout      = 1 * time.Second
 	K8sReachabilityCacheTTL          = 5 * time.Second
 	K8sReachabilityProbeChannelDepth = 1
 	ZeroJitterFraction               = 0.0
 	JitterCenterOffset               = 1.0
 	JitterRangeMultiplier            = 2.0
+	K8sClientRateLimitWaitSub        = "client rate limiter Wait"
 )
